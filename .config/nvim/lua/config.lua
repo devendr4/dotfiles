24f5fcccbe3ad140ff8ea6992384727b08c59c28
devendr4 <angel.emo.altuve@gmail.com>
2023-07-09 23:41:34 -0400

    gitignore fix

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -187,6 +187,6 @@ vim.diagnostic.config({
 })
 
 -- Show line diagnostics automatically in hover window
-vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+ vim.o.updatetime = 250
+ vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
