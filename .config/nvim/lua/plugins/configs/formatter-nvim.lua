2bb8b3a924d443ba78327e62b5b831db537c8672
devendr4 <angel.emo.altuve@gmail.com>
2023-08-15 12:19:42 -0400

    hadolint

--- .config/nvim/lua/plugins/configs/formatter-nvim.lua
+++ .config/nvim/lua/plugins/configs/formatter-nvim.lua
@@ -83,6 +83,7 @@ augroup END
 require("lint").linters_by_ft = {
 	typescript = { "eslint_d" },
 	typescriptreact = { "eslint_d" },
+	dockerfile = { "hadolint" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
