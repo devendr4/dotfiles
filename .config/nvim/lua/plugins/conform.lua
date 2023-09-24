540b89454078e0690e6ceec92589ec1a8eb60706
devendr4 <angel.emo.altuve@gmail.com>
2023-09-24 13:37:10 -0400

    go formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,25 +1,26 @@
 return {
 	"stevearc/conform.nvim",
 	opts = function()
-		local prettier = { "prettierd", "prettier" }
+		local prettier = { "prettierd" } --, "prettier" }
 		return {
 
 			format_on_save = {
 				-- These options will be passed to conform.format()
 				timeout_ms = 500,
-				lsp_fallback = true,
+				-- lsp_fallback = true,
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
 				-- Use a sub-list to run only the first available formatter
-				javascript = { prettier, { "eslint_d", "eslint" } },
+				javascript = { prettier },
 				typescript = { prettier },
 				json = { prettier },
-				vue = { prettier },
+				vue = { prettier, "eslint_d" },
 				html = { prettier },
 				scss = { prettier },
+				go = { "gofumpt" },
 			},
 		}
 	end,
