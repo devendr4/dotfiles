d628dad7ebef0790437d840021a472c754af4f6c
devendr4 <angel.emo.altuve@gmail.com>
2024-08-15 00:28:19 -0400

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+return {
+	color_scheme = "Gruvbox dark, medium (base16)",
+	font_size = 12.0,
+	enable_tab_bar = false,
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
+}
