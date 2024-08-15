5eaca04e2441887e250b5e7d6e93969baafeba84
devendr4 <angel.emo.altuve@gmail.com>
2024-08-15 00:46:19 -0400

    wezterm bar config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,10 @@
-return {
+local wezterm = require("wezterm")
+local config = {
 	color_scheme = "Gruvbox dark, medium (base16)",
 	font_size = 12.0,
-	enable_tab_bar = false,
+	enable_tab_bar = true,
+	tab_bar_at_bottom = true,
+
 	window_padding = {
 		left = 20,
 		right = 20,
@@ -9,3 +12,7 @@ return {
 		bottom = 20,
 	},
 }
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config)
+
+return config
