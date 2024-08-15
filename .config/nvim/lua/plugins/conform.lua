3dbf41d10d63184ab950aae409da43585a095c0c
devendr4 <angel.emo.altuve@gmail.com>
2024-08-15 00:19:39 -0400

    update conform.nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,34 +1,34 @@
 return {
 	"stevearc/conform.nvim",
 	opts = function()
-		local prettier = { "prettierd" } --, "prettier" }
+		local prettier = { "prettierd", "eslint_d" } --, "prettier" }
 		local ruff = { "ruff_fix", "ruff_format" }
 		return {
 
 			format_on_save = {
+
 				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				-- lsp_fallback = true,
-				async = true,
+				-- async = true,
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
-				python = { "ruff_fix", "ruff_format" },
+				python = ruff,
 				-- Use a sub-list to run only the first available formatter
-				javascript = { "eslint_d", prettier },
-				svelte = { "eslint_d", prettier },
-				-- typescript = { prettier },
-				typescript = { "eslintd", "eslint", prettier, "biome" },
-				typescriptreact = { "eslint_d", prettier },
-				javascriptreact = { "eslint_d", prettier },
-				json = { prettier },
-				vue = { prettier, "eslint_d" },
-				html = { prettier },
+				javascript = prettier,
+				svelte = prettier,
+				typescript = prettier,
+				typescriptreact = prettier,
+				javascriptreact = prettier,
+				json = prettier,
+				vue = prettier,
+				html = prettier,
 				-- htmldjango = { "prettier" },
-				markdown = { prettier },
-				scss = { prettier },
-				css = { prettier },
+				markdown = prettier,
+				scss = prettier,
+				css = prettier,
 				go = { "gofumpt" },
 				bash = { "shfmt" },
 				-- sql = { "sqlfmt" },
