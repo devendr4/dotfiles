94de3fd941988ff5957c663dfdb8c2004673bfa1
devendr4 <angel.emo.altuve@gmail.com>
2025-08-04 14:21:27 -0400

    revert folding and tstools

--- .config/nvim/lua/plugins/typescript-tools.lua
+++ .config/nvim/lua/plugins/typescript-tools.lua
@@ -1,21 +1,20 @@
-return {}
--- return {
--- 	"pmizio/typescript-tools.nvim",
---
--- 	config = function()
--- 		require("typescript-tools").setup({
--- 			settings = {
--- 				tsserver_plugins = {
--- 					-- for TypeScript v4.9+
--- 					"@styled/typescript-styled-plugin",
--- 				},
--- 				expose_as_code_action = { "all" },
--- 			},
--- 		})
--- 	end,
---
--- 	-- opts = {
--- 	-- 	tsserver_plugins = { "@styled/typescript-styled-plugin" },
--- 	-- 	expose_as_code_action = { "all" },
--- 	-- },
--- }
+return {
+	"pmizio/typescript-tools.nvim",
+
+	config = function()
+		require("typescript-tools").setup({
+			settings = {
+				tsserver_plugins = {
+					-- for TypeScript v4.9+
+					"@styled/typescript-styled-plugin",
+				},
+				expose_as_code_action = { "all" },
+			},
+		})
+	end,
+
+	-- opts = {
+	-- 	tsserver_plugins = { "@styled/typescript-styled-plugin" },
+	-- 	expose_as_code_action = { "all" },
+	-- },
+}
